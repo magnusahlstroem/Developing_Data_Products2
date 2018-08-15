dde7bc284b7f66fb0fce5befc6894c5f43a58e69
Magnus Glindvad Ahlström <magnus.glindvad.ahlstroem@regionh.dk>
2018-08-15 13:03:54 +0200

    Finished the academic output app

---
+++ Apps/ui.R
@@ -0,0 +1,72 @@
+library(shiny)
+library(readr)
+library(dplyr)
+library(leaflet)
+
+uni <- read_csv("final_data_for_app.csv") %>%
+    arrange(Search) %>%
+    filter(Search != "University Hospital")
+Search_opts <- unique(uni$Search)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Academic output of universities"),
+  #h3("Based on first Author of publications"),
+  
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+        #submitButton("Done"),
+        h4("How to use the app"),
+        p("Enter a start and end date and/or a university and see how many 
+         publications the university published in the chosen period of time, 
+         also you can find a link to the 5 most recent puclications at the 
+         markers. You can also manually search the map and find your 
+         university of choice. Finally if you dont enter a university
+         you can specify how many universities should be displayed.
+         The universities are ranked according to number of publications 
+         during the specified time period."),
+        selectizeInput(
+            'Uni', 'University', choices = Search_opts,
+            selected = "University of Copenhagen",
+            options = list(
+                placeholder = 'Please select a University',
+                onInitialize = I('function() { this.setValue(""); }')
+            )
+            ),
+        dateInput("from", "Start date", 
+                  value = "2018-04-01", 
+                  min = "2018-01-01", 
+                  max = "2018-07-31",
+                  format = "yyyy-mm-dd"),
+        dateInput("to", "End date", 
+                  value = "2018-04-15", 
+                  min = "2018-01-01", 
+                  max = "2018-07-31",
+                  format = "yyyy-mm-dd"),
+        numericInput("no", "How many Universities should be displayed", 
+                     value = 10, min = 1, max = length(Search_opts))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       h3("Documentation"), 
+       p("The app is based on Author affiliations from the medline database, 
+         specifically first author affiliations. From the author affiliation
+         the name of affiliated university was extracted (if available). 
+         Coordinates of universities were extracted with the ggmap R package:"),
+       helpText(   
+           a("Link to ggmap package article", 
+             href="http://journal.r-project.org/archive/2013-1/kahle-wickham.pdf")
+       ),
+       
+       leafletOutput("mapPlot"),
+       br(),
+       tableOutput("table"),
+       div(textOutput("text"), style = "color:red; font-size:125%")
+       )
+  )
+))
